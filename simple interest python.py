09fb5ac45c81cd2b8d317b4f21c8a43ea73a2e17
adnanms699 <adnanms699@gmail.com>
2023-10-15 22:55:14 +0800

    Finding simple interest
    simple interest program

---
+++ simple interest python.py
@@ -0,0 +1,5 @@
+p=int(input("Enter the value of p:"))
+r=int(input("Enter the value of r:"))
+t=int(input("Enter the value of t:"))
+Simpleinterest=(p*r*t)/100
+print("Simpleinterest is :",Simpleinterest)
